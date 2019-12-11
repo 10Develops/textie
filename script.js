fa3c3931a0e0059ef728e76e20d45e0090847ff1
10Develops <32372072+10Develops@users.noreply.github.com>
2019-12-12 03:01:45 +0400

    Site update

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    document.onclick = function(e){
+        if(e.target.className != "dropdown"){
+            document.getElementsByClassName("dropdown-content")[0].style -= "display: block;";
+        }
+    };
+};
+
+function ShowDropdown(){
+    document.getElementsByClassName("dropdown-content")[0].style = "display: block;";
+}
